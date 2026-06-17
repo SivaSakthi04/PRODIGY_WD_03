463b5268f013a15e746079b59b29587b6f2fe348
SIVASAKTHI31 <144511617+SivaSakthi04@users.noreply.github.com>
2026-06-17 21:19:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,128 @@
+const cells = document.querySelectorAll(".cell");
+const statusText = document.getElementById("status");
+const resetBtn = document.getElementById("resetBtn");
+
+let currentPlayer = "X";
+let gameActive = true;
+
+const winPatterns = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
+
+cells.forEach(cell=>{
+    cell.addEventListener("click",handleClick);
+});
+
+resetBtn.addEventListener("click",resetGame);
+
+function handleClick(){
+
+    if(this.textContent !== "" || !gameActive){
+        return;
+    }
+
+    this.textContent = currentPlayer;
+    this.classList.add(currentPlayer.toLowerCase());
+
+    checkWinner();
+
+    if(gameActive){
+        currentPlayer = currentPlayer === "X" ? "O" : "X";
+        statusText.textContent = `Player ${currentPlayer} Turn`;
+    }
+}
+
+function checkWinner(){
+
+    for(let pattern of winPatterns){
+
+        const [a,b,c] = pattern;
+
+        if(
+            cells[a].textContent &&
+            cells[a].textContent === cells[b].textContent &&
+            cells[a].textContent === cells[c].textContent
+        ){
+
+            gameActive = false;
+
+            cells[a].classList.add("winner");
+            cells[b].classList.add("winner");
+            cells[c].classList.add("winner");
+
+            statusText.textContent =
+                `🎉 Player ${cells[a].textContent} Wins!`;
+
+            createFlowers();
+
+            return;
+        }
+    }
+
+    const draw = [...cells].every(
+        cell => cell.textContent !== ""
+    );
+
+    if(draw){
+        gameActive = false;
+        statusText.textContent = "🤝 Draw Match!";
+    }
+}
+function createFlowers(){
+
+    const flowers = [
+        "🌸",
+        "🌺",
+        "🌼",
+        "💐",
+        "🌷"
+    ];
+
+    for(let i=0;i<120;i++){
+
+        const flower =
+            document.createElement("div");
+
+        flower.classList.add("flower");
+
+        flower.innerHTML =
+            flowers[Math.floor(
+                Math.random()*flowers.length
+            )];
+
+        flower.style.left =
+            Math.random()*100 + "vw";
+
+        flower.style.animationDuration =
+            (Math.random()*3 + 2) + "s";
+
+        document.body.appendChild(flower);
+
+        setTimeout(()=>{
+            flower.remove();
+        },5000);
+    }
+
+
+}
+
+function resetGame(){
+
+    currentPlayer = "X";
+    gameActive = true;
+
+    statusText.textContent =
+        "Player X Turn";
+
+    cells.forEach(cell=>{
+        cell.textContent = "";
+        cell.className = "cell";
+    });
+}
